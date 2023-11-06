fb6f44c6d25670b4bc76752ffa46db5eed729a31
kongfuboy123 <kongfuboyjw2@gmail.com>
2023-11-07 02:11:00 +1100

    modify

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,7 +4,7 @@ import Icon from '@/public/icon.png'
 export default function Navbar() {
   return (
     <div className='flex min-w-full bg-blue-400'>
-      <Image src={Icon} width={50} height={50} />
+      <Image src={Icon} width={50} height={50} alt='icon' />
      <h1 className='text-white text-4xl'>WSU Kotlin User Group</h1> 
     </div>
   )
--- app/page.js
+++ app/page.js
@@ -3,7 +3,9 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p className='text-black text-2xl'>Welcome to the WSU Kotlin Meetup Group, a community-driven platform for Kotlin enthusiasts in Western Sydney University! Whether you are a beginner or an experienced developer, this group aims to foster networking, learning, and collaboration among Kotlin enthusiasts. Join us to share ideas, explore innovative Kotlin projects, and stay up-to-date with the latest trends and advancements in the Kotlin ecosystem. Let's come together and empower each other as we celebrate our love for Kotlin!</p>
+      <p className='text-black text-2xl whitespace-pre-wrap'>
+        <span className='font-bold text-3xl'>Welcome to the WSU Kotlin User Group,</span>  a community-driven platform for Kotlin enthusiasts in Western Sydney University!{"\n"}
+        Whether you are a beginner or an experienced developer, this group aims to foster networking, learning, and collaboration among Kotlin enthusiasts. Join us to share ideas, explore innovative Kotlin projects, and stay up-to-date with the latest trends and advancements in the Kotlin ecosystem. Let's come together and empower each other as we celebrate our love for Kotlin! </p>
     </main>
   )
 }
